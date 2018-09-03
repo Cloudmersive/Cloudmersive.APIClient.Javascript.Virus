b94147b4f3d061027dcbee26001e98c6168c9c73
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2018-09-03 02:45:26 -0700

    Added

---
+++ browserify/main.js
@@ -0,0 +1,3 @@
+﻿var CloudmersiveConvertApiClient = require('cloudmersive-convert-api-client');
+var CloudmersiveValidateApiClient = require('cloudmersive-validate-api-client');
+var CloudmersiveImageApiClient = require('cloudmersive-image-api-client');
